72fb0c4e51638c13a16c31a4d148b6d8ba439a9c
Bang28 <mhdmasruri9@gmail.com>
2024-08-08 15:22:45 +0700

    Django signal initiation

--- ptkr/ptkr/models/shp.py
+++ ptkr/ptkr/models/shp.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 import datetime
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 
 class Shp(models.Model):
     name = models.CharField(_("Nama Peta"), max_length=50)
@@ -13,3 +16,12 @@ class Shp(models.Model):
     
     class Meta:
         verbose_name_plural = 'Shape Files'
+
+
+@receiver(post_save, sender=Shp)
+def publish_date(sender, instance, created, **kwargs):
+    pass
+
+@receiver(post_delete, sender=Shp)
+def delete_date(sender, instance, created, **kwargs):
+    pass
