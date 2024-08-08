5a9c0d7fecd34d1ca3767681fe32943a636bc910
Bang28 <mhdmasruri9@gmail.com>
2024-08-08 15:39:00 +0700

    upload shapefile to postgis using django signal

--- ptkr/ptkr/models/shp.py
+++ ptkr/ptkr/models/shp.py
@@ -3,7 +3,20 @@ from django.utils.translation import gettext_lazy as _
 import datetime
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+import geopandas as gpd
+import os
+import glob
+import zipfile
+from sqlalchemy import *
+from geo.Geoserver import Geoserver
+from pg.pg import Pg 
 
+# Initialize the library
+db = Pg(dbname='ptkr', user='postgres', password='12345678', host='localhost', port='5433')
+geo = Geoserver('http://127.0.0.1:8080/geoserver', username='admin', password='geoserver')
+
+# Database connection string (postgresql://${database_user}:${databse_password}@${database_host}:${database_port}/${database_name}
+conn_str = 'postgresql://postgres:12345678@localhost:5433/ptkr'
 
 class Shp(models.Model):
     name = models.CharField(_("Nama Peta"), max_length=50)
@@ -20,7 +33,41 @@ class Shp(models.Model):
 
 @receiver(post_save, sender=Shp)
 def publish_date(sender, instance, created, **kwargs):
-    pass
+    file = instance.file.path
+    file_format = os.path.basename(file).split('.')[-1]
+    file_name = os.path.basename(file).split('.')[0]
+    file_path = os.path.dirname(file)
+    name = instance.name
+
+    # extract zipfile
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(file_path)
+
+    os.remove(file) # remove zip file
+
+    shp = glob.glob(r'{}/**/*.shp'.format(file_path), 
+                    recursive=True) # to get shp
+
+    try:
+        req_shp = shp[0]
+        gdf = gpd.read_file(req_shp) #make geodataframe
+        engine = create_engine(conn_str) # create the SQLAlchemy's engine to use
+        gdf.to_postgis(
+            con=engine,
+            schema='data',
+            name=name,
+            if_exists='replace'
+        )
+
+        for s in shp:
+            os.remove(s)
+
+    except Exception as e:
+        for s in shp:
+            os.remove(s)
+
+        instance.delete()
+        print('There is problem during shp uploade: ', e)
 
 @receiver(post_delete, sender=Shp)
 def delete_date(sender, instance, created, **kwargs):
