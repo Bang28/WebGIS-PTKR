7aedef1a43772614c6fae4380a741ab8c5b866b4
Bang28 <mhdmasruri9@gmail.com>
2024-08-28 09:13:19 +0700

    feat: statistik kejadian bencana bulanan

--- ptkr/ptkr/views/views_statistik.py
+++ ptkr/ptkr/views/views_statistik.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render
 from django.db.models import Count
-from ptkr.models.ptkr import Bencana, RumahTerdampak
+from django.db.models.functions import TruncMonth
 from collections import defaultdict
 import json
+from ptkr.models.ptkr import Bencana, RumahTerdampak
 
+def get_data_rumah_terdampak():
+    """Mengambil semua data rumah terdampak bencana"""
+    return RumahTerdampak.objects.all().order_by('-publish')
 
-def statistik(request):
-    """fungsi menampilkan statistik bencana dan rumah terdampak"""
+def get_data_list_bencana():
+    """Mengambil semua data bencana"""
+    return Bencana.objects.all().order_by('-tanggal_terjadi')
 
-    # ambil semua data rumah terdampak bencana
-    all_data = RumahTerdampak.objects.all().order_by('-publish')
-    list_bencana = Bencana.objects.all().order_by('-tanggal_terjadi')
-    # Query untuk statistik rumah terdampak berdasarkan bencana yang terjadi, termasuk tingkat kerusakan dan tanggal kejadian
-    rumah_terdampak_bencana = RumahTerdampak.objects.values(
+def get_statistik_rumah_terdampak():
+    """Mengambil statistik rumah terdampak berdasarkan bencana yang terjadi"""
+    return RumahTerdampak.objects.values(
         'bencana__jenis_bencana', 'bencana__tanggal_terjadi', 'tingkat_kerusakan'
     ).annotate(jumlah=Count('id')).order_by('bencana__jenis_bencana', 'tingkat_kerusakan')
 
-    # Query statistik kejadian bencana berdasarkan tanggal kejadian dan jenis bencana
-    kejadian_bencana = Bencana.objects.values('tanggal_terjadi', 'jenis_bencana').annotate(jumlah=Count('id')).order_by('tanggal_terjadi')
+def get_statistik_kejadian_bencana():
+    """Mengambil statistik kejadian bencana berdasarkan tanggal kejadian dan jenis bencana"""
+    return Bencana.objects.values('tanggal_terjadi', 'jenis_bencana').annotate(jumlah=Count('id')).order_by('tanggal_terjadi')
 
+def get_statistik_kejadian_bencana_per_bulan_jenis():
+    """Mengambil statistik kejadian bencana berdasarkan bulan dan jenis bencana"""
+    return Bencana.objects.annotate(month=TruncMonth('tanggal_terjadi')).values('month', 'jenis_bencana').annotate(jumlah=Count('id')).order_by('month', 'jenis_bencana')
+
+
+def prepare_data_for_chart(rumah_terdampak_bencana, kejadian_bencana):
+    """Menyiapkan data untuk chart berdasarkan rumah terdampak dan kejadian bencana"""
+    
     # Menyiapkan list unik dari 'jenis_bencana' + 'tanggal_terjadi'
     kerusakan_rumah_labels = sorted(
         set(f"{entry['bencana__jenis_bencana']} ({entry['bencana__tanggal_terjadi'].strftime('%Y-%m-%d')})" 
@@ -48,8 +60,13 @@ def statistik(request):
         jenis = entry['jenis_bencana']
         jumlah = entry['jumlah']
         data_per_tanggal[tanggal][jenis] = jumlah
-    
-    # Siapkan data untuk Chart.js
+
+    return kerusakan_rumah_labels, data_ringan, data_sedang, data_berat, data_per_tanggal, jenis_bencana_list
+
+def prepare_chart_data(kerusakan_rumah_labels, data_ringan, data_sedang, data_berat, data_per_tanggal, jenis_bencana_list):
+    """Menyiapkan data untuk Chart.js"""
+
+    # statistik kerusakan rumah akibat kejadian bencana per tanggal kejadian
     chart_data_kerusakan_rumah = {
         'labels': kerusakan_rumah_labels,
         'datasets': [
@@ -71,6 +88,7 @@ def statistik(request):
         ]
     }
 
+    # statistik kejadian bencana per tanggal kejadian
     # Membentuk struktur data untuk Chart.js
     labels = sorted(data_per_tanggal.keys())
     datasets = []
@@ -100,13 +118,54 @@ def statistik(request):
         'datasets': datasets
     }
 
-    # mengambil data rumah terdampak berdasarkan bencana
+    return chart_data_kerusakan_rumah, chart_data_kejadian_bencana
+
+def prepare_bulanan_chart_data_per_jenis(kejadian_bencana_per_bulan_jenis):
+    """Menyiapkan data untuk chart statistik kejadian bencana berdasarkan bulan dan jenis bencana"""
+
+    data_per_jenis = defaultdict(lambda: defaultdict(int))
+    for entry in kejadian_bencana_per_bulan_jenis:
+        month = entry['month'].strftime('%Y-%m')
+        jenis = entry['jenis_bencana']
+        jumlah = entry['jumlah']
+        data_per_jenis[month][jenis] = jumlah
+
+    # Menyiapkan data untuk Chart.js
+    labels = sorted(data_per_jenis.keys())
+    jenis_bencana_list = sorted(set(jenis for jenis_dict in data_per_jenis.values() for jenis in jenis_dict.keys()))
+
+    datasets = []
+    background_colors = {
+        'Gempa Bumi': 'rgba(255, 99, 132, 0.5)',
+        'Banjir': 'rgba(54, 162, 235, 0.5)',
+        'Topan': 'rgba(255, 206, 86, 0.5)',
+        'Kebakaran': 'rgba(75, 192, 192, 0.5)',
+        'Tanah Bergerak': 'rgba(153, 102, 255, 0.5)',
+        'Tanah Longsor': 'rgba(255, 159, 64, 0.5)'
+    }
+
+    for jenis_bencana in jenis_bencana_list:
+        data = [data_per_jenis[month].get(jenis_bencana, 0) for month in labels]
+        datasets.append({
+            'label': jenis_bencana,
+            'data': data,
+            'backgroundColor': background_colors.get(jenis_bencana, 'rgba(0, 0, 0, 0.5)')
+        })
+
+    chart_data_bulanan = {
+        'labels': labels,
+        'datasets': datasets
+    }
+    
+    return chart_data_bulanan
+
+def get_per_bencana_data(list_bencana):
+    """Mengambil data rumah terdampak berdasarkan bencana"""
+    
     per_bencana = []
     for bencana in list_bencana:
-        # Memastikan 'bencana' adalah instance yang valid dari model Bencana
         rumah_terdampak = RumahTerdampak.objects.filter(bencana=bencana)
 
-        # Periksa jika ada rumah terdampak yang terkait
         if rumah_terdampak.exists():
             ringan = rumah_terdampak.filter(tingkat_kerusakan='Rusak Ringan').count()
             sedang = rumah_terdampak.filter(tingkat_kerusakan='Rusak Sedang').count()
@@ -121,11 +180,28 @@ def statistik(request):
                 'berat': berat,
                 'id': bencana.id
             })
+    return per_bencana
+
+def statistik(request):
+    """Fungsi utama untuk menampilkan statistik bencana dan rumah terdampak"""
+    
+    all_data = get_data_rumah_terdampak()
+    list_bencana = get_data_list_bencana()
+    rumah_terdampak_bencana = get_statistik_rumah_terdampak()
+    kejadian_bencana = get_statistik_kejadian_bencana()
+    kejadian_bencana_per_bulan_jenis = get_statistik_kejadian_bencana_per_bulan_jenis()
+    
+    kerusakan_rumah_labels, data_ringan, data_sedang, data_berat, data_per_tanggal, jenis_bencana_list = prepare_data_for_chart(rumah_terdampak_bencana, kejadian_bencana)
+    chart_data_kerusakan_rumah, chart_data_kejadian_bencana = prepare_chart_data(kerusakan_rumah_labels, data_ringan, data_sedang, data_berat, data_per_tanggal, jenis_bencana_list)
+    chart_data_bulanan_jenis = prepare_bulanan_chart_data_per_jenis(kejadian_bencana_per_bulan_jenis)
+    per_bencana = get_per_bencana_data(list_bencana)
     
     context = {
         'list_semua_data': all_data,
         'list_per_bencana': per_bencana,
         'chart_data_kerusakan_rumah': json.dumps(chart_data_kerusakan_rumah),
         'chart_data_kejadian_bencana': json.dumps(chart_data_kejadian_bencana),
+        'chart_data_bulanan': json.dumps(chart_data_bulanan_jenis),
     }
+    
     return render(request, 'statistik.html', context)
