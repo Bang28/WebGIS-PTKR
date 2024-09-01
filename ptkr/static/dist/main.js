428f168f13ed54331b9609e58809a25e2aa2a332
Bang28 <mhdmasruri9@gmail.com>
2024-09-01 19:49:10 +0700

    collecstatic and depedensi requirements

--- ptkr/static/dist/main.js
+++ ptkr/static/dist/main.js
@@ -1,5 +1,5 @@
 // map class initialize
-var map = L.map('map').setView([38.8610, 71.2761], 7);
+var map = L.map('map').setView([-1.5387473, 118.4170327], 5);
 map.zoomControl.setPosition('topright'); //set position zoom control to topright "by default topleft"
 
 // adding osm layer
@@ -7,28 +7,9 @@ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var watercolorMap = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
-    minZoom: 1,
-    maxZoom: 16,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'jpg'
-});
-
-var st = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 0,
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
-});
-
 var ewi = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
-
-// adding marker in the center of map
-var singleMarker = L.marker([38.8610, 71.2761])
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
 
 // add scale
 L.control.scale({
@@ -40,31 +21,8 @@ map.on('mousemove', function (e) {
     $('.coordinate').html('Lat: ' + e.latlng.lat + ' Long: ' + e.latlng.lng)
 });
 
-// geoJson load
-// L.geoJSON(data).addTo(map);
-var marker = L.markerClusterGroup();
-var taji = L.geoJSON(data, {
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.name)
-    }
-});
-taji.addTo(marker);
-marker.addTo(map); // default 
-
 // leaflet layer control
 var baseMaps = {
     'OSM': osm,
-    'Water Color Maps': watercolorMap,
-    'Stamen Toner': st,
-    'Esri World Imagery': ewi
+    'Esri World Imagery': ewi,
 }
-
-var overlayMaps = {
-    'GeoJSON Marker': marker, // default marker value when i uncomment marker in GeoJso load
-    'Single Marker': singleMarker
-}
-
-L.control.layers(baseMaps, overlayMaps, {
-    collapse: false,
-    position: 'topleft', // default topright    
-}).addTo(map);
--- ptkr/static/dist/web-GIS.js
+++ ptkr/static/dist/web-GIS.js
@@ -1,30 +1,22 @@
-// full screen map view
-var mapId = document.getElementById('map');
-function fullScreenView() {
-    if (document.fullscreenElement){
-        document.exitFullscreen()
-    }else{
-        mapId.requestFullscreen();
-    }
-}
-
-// map print with ext library "leaflet browser print function"
-L.control.browserPrint({
-    position: 'topright', // default topleft
-}).addTo(map)
-
 // leaflet search 
-L.Control.geocoder().addTo(map);
-
-// leaflet measure
-L.control.measure({
-    primaryLengthUnit: 'kilometers',
-    secondaryLengthUnit: 'meters',
-    primaryAreaUnit: 'sqmeters',
-    secondaryAreaUnit: undefined
+L.Control.geocoder({
+    position: 'topleft',
+    collapsed: false,
+    placeholder: 'Cari Lokasi...',
 }).addTo(map);
 
 // zoom to layer function
 $('.zoom-to-layer').click(function () {
-    map.setView([38.8610, 71.2761], 7)
+    map.setView([-7.204620857883746, 109.0623553088576], 14)
 });
+
+// get locate
+L.control.locate({
+    position: 'topright',
+    strings: {
+        title: "Tampilkan lokasi anda"
+    },
+    locateOptions: {
+        enableHighAccuracy: true
+    }
+}).addTo(map);
