d730534e8caa505f9e41707ad757053d6434e571
Bang28 <mhdmasruri9@gmail.com>
2024-09-03 18:49:26 +0700

    update urls routing

--- ptkr/core/static/dist/main.js
+++ ptkr/core/static/dist/main.js
@@ -3,13 +3,9 @@ var map = L.map('map').setView([-1.5387473, 118.4170327], 5);
 map.zoomControl.setPosition('topright'); //set position zoom control to topright "by default topleft"
 
 // adding osm layer
-var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-var ewi = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
+var ewi = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}');
 
 // add scale
 L.control.scale({
--- ptkr/ptkr/urls.py
+++ ptkr/ptkr/urls.py
@@ -9,12 +9,12 @@ from .views.views_statistik import statistik
 
 app_name = 'ptkr'
 urlpatterns = [
-    path('beranda/', beranda, name='beranda'),
-    path('satu-lantai/', satuLantai, name='satu'),
-    path('dua-lantai/', duaLantai, name='dua'),
-    path('tiga-lantai/', tigaLantai, name='tiga'),
+    path('', beranda, name='beranda'),
+    path('form-input/satu-lantai/', satuLantai, name='satu'),
+    path('form-input/dua-lantai/', duaLantai, name='dua'),
+    path('form-input/tiga-lantai/', tigaLantai, name='tiga'),
     path('statistik/', statistik, name='statistik'),
-    path('detail/<id>', detailRumah, name='detail-rumah'),
+    path('detail-rumah/<id>', detailRumah, name='detail-rumah'),
     path('sign-in/', signIn, name='sign-in'),
     path('sign-out/', signOut, name='sign-out'),
     path('detail-bencana/<id>', detailBencana, name='detail-bencana'),
